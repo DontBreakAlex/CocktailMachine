99864b6fb63261892330231931d1f4ddb907f19e
Alexandre Seo <perso@alexandre-seo.tk>
2019-04-22 16:38:08 +0200

    processing acquired jobs with handbrake

--- slave.js
+++ slave.js
@@ -1,14 +1,19 @@
 const axios = require('axios');
+const exec = require('child_process').exec;
+const outputpath = '/home/alexandre/cocktailmachineoutput'
+const execpath = '/home/alexandre/Vidéos'
 
 
 
 axios({
     url: 'http://127.0.0.1:3000/job',
     method: 'GET',
-}).catch(err => console.log(err)).then(response=> {
-    axios({
-    url: 'http://127.0.0.1:3000/job',
-    method: 'PUT',
-    data: {uuid: response.data[1]}
-}).catch(err => console.log(err))
+}).catch(err => console.log(err)).then(response=> { // TODO: Cerate directory before starting job, otherwise handbrake crashes
+    exec(`HandBrakeCLI -i ${execpath+response.data[0]} -o ${outputpath+response.data[0]}`, (stdout, stderr) => {
+        axios({
+            url: 'http://127.0.0.1:3000/job',
+            method: 'PUT',
+            data: {uuid: response.data[1]} 
+        }).catch(err => console.log(err))
+    })
 })
