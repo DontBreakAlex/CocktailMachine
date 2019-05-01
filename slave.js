e4e995f1475ea4606f28131744cb1b1db344db0b
Alexandre Seo <perso@alexandre-seo.tk>
2019-05-01 12:01:57 +0200

    It turns out child_process.spawn can also have buffer issues

--- slave.js
+++ slave.js
@@ -10,7 +10,7 @@ async function Work(num, execoptions, execpath, outputpath) {
         let job = await axios({ // Ask the master for a job
             url: `http://${ip}/job`,
             method: 'GET',
-        }).catch(err => error = err)
+        }).catch(err => error = err);
         if (job.data == undefined) {console.log(`Thread ${num} exiting: no more jobs`); return 'done';} // If no more jobs are available, stop the worker
         await new Promise(async (resolve, reject) => {
             let index  = job.data[0].lastIndexOf('/'); // Check the file is in a directory, and if yes, create it
@@ -22,29 +22,31 @@ async function Work(num, execoptions, execpath, outputpath) {
                     })
                 }).catch(err => console.warn(err));
             }
-            let noext = job.data[0].substring(0, job.data[0].lastIndexOf('.'))
-            let arg = execoptions.split(' ')
-            arg.push('-i', execpath + job.data[0], '-o', outputpath + noext + '.mp4')
-            let HandBrake = spawn('HandBrakeCLI', arg) // Start HandBrake
-            Exit(HandBrake, num, job, resolve, reject) // Handle HandBrake exit
+            let noext = job.data[0].substring(0, job.data[0].lastIndexOf('.'));
+            let arg = execoptions.split(' ');
+            arg.push('-i', execpath + job.data[0], '-o', outputpath + noext + '.mp4');
+            let HandBrake = spawn('HandBrakeCLI', arg, {stdio: 'ignore'}); // Start HandBrake
+            Exit(HandBrake, num, job, resolve, reject); // Handle HandBrake exit
         }).catch(err => console.warn(err));
     }
 }
 
 function Exit(HandBrake, num, job, next, fail) { 
-    HandBrake.on('exit', (code) => {
+    HandBrake.on('exit', code => {
         if (code == 0) {
-            console.log(`Thread ${num} has finished processing ${job.data[0]}`)
+            console.log(`Thread ${num} has finished processing ${job.data[0]}`);
         } else {
-            console.warn(`Thread ${num} has failed processing ${job.data[0]}`)
+            console.warn(`Thread ${num} has failed processing ${job.data[0]}`);
         }
         axios({ // Tell the server that the job is finished
             url: `http://${ip}/job`,
             method: 'PUT',
             data: { uuid: job.data[1], status: code }
         }).catch(err => {error = err; fail(err)}).then(next());
-    })
-    /* HandBrake.stdout.on('data', data => console.log(`${num}: ${data.toString().substring(24)}`)) */ // Uncomment to display progress (not recommended)
+    });
+    // If you want to use these, you need to comment out (or delete) ", {stdio: 'ignore'}" at line 29
+    //HandBrake.stdout.on('data', data => {console.log(`Thread ${num}: ${data.toString().substring(24,30)}`)}); // Uncomment to see HandBrake progression (not recommended: spits out a lot of data and is mostly unreadable)
+    //HandBrake.stderr.on('data', data => {console.log(`Thread ${num}, stderr: ${data}`)}); // Uncomment at your own risk
 }
 
 axios({ // Get settings from the master
@@ -52,4 +54,4 @@ axios({ // Get settings from the master
     method: 'GET',
 }).then(data => {
     for (i=0; i<os.cpus().length/data.data.coresperthread; ++i) {Work(i, data.data.execoptions, data.data.execpath, data.data.outputpath); console.log(`Starting thread ${i}`);} // Start workers
-})
+});
